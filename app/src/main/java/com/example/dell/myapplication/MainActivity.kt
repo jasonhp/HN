39adced4d37f49b93ed02ea7769c98b49c2a86a4
Jason <ggbbddjm@gmail.com>
2018-07-30 14:27:52 +0800

    finish listview

--- app/src/main/java/com/example/dell/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/dell/myapplication/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.dell.myapplication
 
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dell.myapplication.dummy.DummyContent
 import java.util.*
 import kotlin.concurrent.schedule
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ArticleListFragment.OnListFragmentInteractionListener, ArticleReaderFragment.OnFragmentInteractionListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Timer().schedule(3000){
             val listFragment = ArticleListFragment()
-            val transaction = getSupportFragmentManager().beginTransaction()
+            val transaction = supportFragmentManager.beginTransaction()
             transaction.replace(R.id.fragment_container, listFragment)
             transaction.commit()
+
+//            listFragment.setOnListFragmentInteractionListener(object: ArticleListFragment.OnListFragmentInteractionListener {
+//                override fun onListFragmentInteraction(item: DummyContent.DummyItem?) {
+//                    val readerFragment = ArticleReaderFragment()
+//                    val transaction = supportFragmentManager.beginTransaction()
+//                    transaction.replace(R.id.fragment_container, readerFragment)
+//                    transaction.commit()
+//                }
+//            })
         }
+    }
+
+    override fun onListFragmentInteraction(item: DummyContent.DummyItem?) {
+        val readerFragment = ArticleReaderFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, readerFragment)
+        transaction.commit()
+    }
+
+    override fun onFragmentInteraction(uri: Uri) {
+        print(uri)
     }
 }
